c68a99d88db551df15fb1771ebaab23a2e83938e
Umut Erozan <umut_erozan@hotmail.com>
2023-03-24 22:55:17 +0300

    hotfix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,8 +2,21 @@ import { BiFullscreen } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai"
 import { MdOutlineSwapVert } from "react-icons/md"
 import {IChannel} from "@/types/IChannel";
+import {useState} from "react";
 
 export default function Sidebar({ selectedChannels, setSelectedChannels, unselectedChannels, setUnselectedChannels, isSidebarOpen, setSidebarOpen }: any) {
+    const [isFullScreen, setFullScreen] = useState(false)
+
+    const handleFullScreen = async () => {
+        if (!isFullScreen) {
+            await document.body.requestFullscreen()
+            setFullScreen(true)
+        }
+        if (isFullScreen) {
+            await document.exitFullscreen()
+            setFullScreen(false)
+        }
+    }
 
     const handleChannel = (channel: IChannel) => {
         if (channel.isSelected === false) {
@@ -24,45 +37,45 @@ export default function Sidebar({ selectedChannels, setSelectedChannels, unselec
     }
 
     return (
-        <div className={`p-6 bg-[#212529] text-white h-screen w-[400px] absolute text-lg overflow-y-auto z-10 ${isSidebarOpen ? "-translate-x-0" : "-translate-x-full"}`}>
+        <div className={`p-6 bg-[#245953] text-white h-screen w-[400px] absolute text-lg overflow-y-auto z-10 transition-all ${isSidebarOpen ? "-translate-x-0" : "-translate-x-full"}`}>
             <div className="flex items-center justify-between">
                 <h1>Mantenna | Kanallar ({selectedChannels.length})</h1>
                 <div className="flex items-center gap-4">
-                    <button><BiFullscreen className="text-gray-400" size={32}/></button>
-                    <button onClick={() => setSidebarOpen(false)}><AiOutlineClose className="text-gray-400" size={32}/></button>
+                    <button onClick={handleFullScreen}><BiFullscreen className={isFullScreen ? "text-[#D8D8D8] hover:text-gray-400" : "text-gray-400 hover:text-[#D8D8D8]"} size={32}/></button>
+                    <button onClick={() => setSidebarOpen(false)}><AiOutlineClose className="text-gray-400 hover:text-[#D8D8D8]" size={32}/></button>
                 </div>
             </div>
 
             <div className="grid gap-y-8">
-                    <div className="mt-4">
-                        <div className="grid gap-y-2">
-                            {selectedChannels.map((channel: IChannel, key: any) => (
-                                <div key={key} className="flex items-center justify-between border rounded h-12 bg-gray-100 text-black">
-                                    <div className="flex items-center h-full w-full">
-                                        <button className="bg-gray-300 h-full px-4"><MdOutlineSwapVert /></button>
-                                        <h2 className="flex items-center h-full w-full pl-4">{channel.title}</h2>
-                                    </div>
-                                    <button className="bg-gray-300 h-full px-4" onClick={() => handleChannel(channel)}>Drop</button>
+                <div className="mt-4">
+                    <div className="grid gap-y-2">
+                        {selectedChannels.map((channel: IChannel, key: any) => (
+                            <div key={key} className="flex items-center justify-between border rounded h-12 bg-gray-100 text-black">
+                                <div className="flex items-center h-full w-full">
+                                    <button className="bg-[#D8D8D8] h-full px-4"><MdOutlineSwapVert /></button>
+                                    <h2 className="flex items-center h-full w-full pl-4">{channel.title}</h2>
                                 </div>
-                            ))}
-                        </div>
-                    </div>
-
-                    <div className="grid gap-y-4">
-                        <h1>Gösterilmeyen Kanallar ({unselectedChannels.length})</h1>
-                        <div className="grid gap-y-2">
-                            {unselectedChannels.map((channel: IChannel, key: any) => (
-                                <div key={key} className="flex items-center justify-between border rounded h-12 bg-gray-100 text-black">
-                                    <div className="flex items-center h-full w-full">
-                                        <button className="bg-gray-300 h-full px-4"><MdOutlineSwapVert /></button>
-                                        <h2 className="flex items-center h-full w-full pl-4">{channel.title}</h2>
-                                    </div>
-                                    <button className="bg-gray-300 h-full px-4" onClick={() => handleChannel(channel)}>Add</button>
-                                </div>
-                            ))}
-                        </div>
+                                <button className="bg-[#D8D8D8] h-full px-4" onClick={() => handleChannel(channel)}>Drop</button>
+                            </div>
+                        ))}
                     </div>
                 </div>
+
+                <div className="grid gap-y-4">
+                    <h1>Gösterilmeyen Kanallar ({unselectedChannels.length})</h1>
+                    <div className="grid gap-y-2">
+                        {unselectedChannels.map((channel: IChannel, key: any) => (
+                            <div key={key} className="flex items-center justify-between border rounded h-12 bg-gray-100 text-black">
+                                <div className="flex items-center h-full w-full">
+                                    <button className="bg-[#D8D8D8] h-full px-4"><MdOutlineSwapVert /></button>
+                                    <h2 className="flex items-center h-full w-full pl-4">{channel.title}</h2>
+                                </div>
+                                <button className="bg-[#D8D8D8] h-full px-4" onClick={() => handleChannel(channel)}>Add</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Layout, Channel, Sidebar } from "@/components";
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 import { IChannel } from "@/types/IChannel";
 import { getChannels } from "@/service/channels";
 import { GiTvRemote } from "react-icons/gi"
@@ -19,13 +19,16 @@ export default function Channels() {
 
     return (
         <Layout title="Channels">
-            <button onClick={() => setSidebarOpen(true)} className="absolute bg-gray-300 h-full flex items-center justify-center"><GiTvRemote size={54} /></button>
             <Sidebar selectedChannels={selectedChannels} setSelectedChannels={setSelectedChannels} unselectedChannels={unselectedChannels} setUnselectedChannels={setUnselectedChannels} isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} />
             <div onClick={() => setSidebarOpen(false)} className={`fixed w-screen h-screen bg-black opacity-50 ${isSidebarOpen ? "block" : "hidden"}`}></div>
-            <div className={`channels_container h-screen gap-2 grid grid-cols-${parseInt(selectedChannels.length / 2)}`}>
-                {selectedChannels.map((channel, key) => (
-                    <Channel key={key} title={channel.title} address={channel.address} />
-                ))}
+            <div className="flex items-center">
+                <button onClick={() => setSidebarOpen(true)} className="bg-[#408E91] h-screen"><GiTvRemote className="text-[#D8D8D8]" size={32} /></button>
+                <div className={`bg-gray-100 channels_container h-screen w-full gap-1 p-1 grid grid-cols-2`}>
+                    {selectedChannels.map((channel, key) => (
+                        <Channel key={key} title={channel.title} address={channel.address} />
+                    ))}
+                </div>
+                <button onClick={() => setSidebarOpen(true)} className="bg-[#408E91] h-screen"><GiTvRemote className="text-[#D8D8D8]" size={32} /></button>
             </div>
         </Layout>
     )
