fbce30dc843e78c46f0fae322d54efa1d1ff94fb
Umut Erozan <umut_erozan@hotmail.com>
2023-03-24 17:18:03 +0300

    Created state and implemented

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,9 @@
-import {BiFullscreen} from "react-icons/bi";
-import {AiOutlineClose} from "react-icons/ai"
+import { BiFullscreen } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai"
+import { MdOutlineSwapVert } from "react-icons/md"
 import {IChannel} from "@/types/IChannel";
 
-export default function Sidebar({ selectedChannels, setSelectedChannels, unselectedChannels, setUnselectedChannels }: any) {
+export default function Sidebar({ selectedChannels, setSelectedChannels, unselectedChannels, setUnselectedChannels, isSidebarOpen, setSidebarOpen }: any) {
 
     const handleChannel = (channel: IChannel) => {
         if (channel.isSelected === false) {
@@ -23,33 +24,45 @@ export default function Sidebar({ selectedChannels, setSelectedChannels, unselec
     }
 
     return (
-        <div className="p-6 bg-[#212529] text-white h-screen w-[300px] absolute text-lg">
+        <div className={`p-6 bg-[#212529] text-white h-screen w-[400px] absolute text-lg overflow-y-auto z-10 ${isSidebarOpen ? "-translate-x-0" : "-translate-x-full"}`}>
             <div className="flex items-center justify-between">
-
-                <h1>Mantenna | Settings</h1>
-                <div className="flex items-center gap-2">
-                    <button><BiFullscreen className="text-gray-400" size={24}/></button>
-                    <button><AiOutlineClose className="text-gray-400" size={24}/></button>
-                </div>
-
-            </div>
-
-            <div className="mt-20 grid gap-y-4">
-                <h1>Channel List</h1>
-                <div className="grid gap-y-4">
-                    <div className="grid gap-y-2">
-                        {selectedChannels.map((channel: IChannel, key: any) => (
-                            <button onClick={() => handleChannel(channel)} key={key} className="w-full bg-gray-100 text-black">{channel.title}</button>
-                        ))}
-                    </div>
-                    <hr/>
-                    <div className="grid gap-y-2">
-                        {unselectedChannels.map((channel: IChannel, key: any) => (
-                            <button onClick={() => handleChannel(channel)} key={key} className="w-full bg-gray-100 text-black">{channel.title}</button>
-                        ))}
-                    </div>
+                <h1>Mantenna | Kanallar ({selectedChannels.length})</h1>
+                <div className="flex items-center gap-4">
+                    <button><BiFullscreen className="text-gray-400" size={32}/></button>
+                    <button onClick={() => setSidebarOpen(false)}><AiOutlineClose className="text-gray-400" size={32}/></button>
                 </div>
             </div>
+
+            <div className="grid gap-y-8">
+                    <div className="mt-4">
+                        <div className="grid gap-y-2">
+                            {selectedChannels.map((channel: IChannel, key: any) => (
+                                <div key={key} className="flex items-center justify-between border rounded h-12 bg-gray-100 text-black">
+                                    <div className="flex items-center h-full w-full">
+                                        <button className="bg-gray-300 h-full px-4"><MdOutlineSwapVert /></button>
+                                        <h2 className="flex items-center h-full w-full pl-4">{channel.title}</h2>
+                                    </div>
+                                    <button className="bg-gray-300 h-full px-4" onClick={() => handleChannel(channel)}>Drop</button>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="grid gap-y-4">
+                        <h1>Gösterilmeyen Kanallar ({unselectedChannels.length})</h1>
+                        <div className="grid gap-y-2">
+                            {unselectedChannels.map((channel: IChannel, key: any) => (
+                                <div key={key} className="flex items-center justify-between border rounded h-12 bg-gray-100 text-black">
+                                    <div className="flex items-center h-full w-full">
+                                        <button className="bg-gray-300 h-full px-4"><MdOutlineSwapVert /></button>
+                                        <h2 className="flex items-center h-full w-full pl-4">{channel.title}</h2>
+                                    </div>
+                                    <button className="bg-gray-300 h-full px-4" onClick={() => handleChannel(channel)}>Add</button>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import { Layout, Channel, Sidebar } from "@/components";
 import {useState, useEffect} from "react";
 import { IChannel } from "@/types/IChannel";
 import { getChannels } from "@/service/channels";
+import { GiTvRemote } from "react-icons/gi"
 
 export default function Channels() {
 
     const [selectedChannels, setSelectedChannels] = useState<IChannel[]>([])
     const [unselectedChannels, setUnselectedChannels] = useState<IChannel[]>([])
+    const [isSidebarOpen, setSidebarOpen] = useState(false)
 
     useEffect(() => {
         getChannels().then((res: any) => {
@@ -17,8 +19,10 @@ export default function Channels() {
 
     return (
         <Layout title="Channels">
-            <Sidebar selectedChannels={selectedChannels} setSelectedChannels={setSelectedChannels} unselectedChannels={unselectedChannels} setUnselectedChannels={setUnselectedChannels} />
-            <div className="channels_container h-screen gap-2 grid grid-cols-2">
+            <button onClick={() => setSidebarOpen(true)} className="absolute bg-gray-300 h-full flex items-center justify-center"><GiTvRemote size={54} /></button>
+            <Sidebar selectedChannels={selectedChannels} setSelectedChannels={setSelectedChannels} unselectedChannels={unselectedChannels} setUnselectedChannels={setUnselectedChannels} isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} />
+            <div onClick={() => setSidebarOpen(false)} className={`fixed w-screen h-screen bg-black opacity-50 ${isSidebarOpen ? "block" : "hidden"}`}></div>
+            <div className={`channels_container h-screen gap-2 grid grid-cols-${parseInt(selectedChannels.length / 2)}`}>
                 {selectedChannels.map((channel, key) => (
                     <Channel key={key} title={channel.title} address={channel.address} />
                 ))}
